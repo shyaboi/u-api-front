64d6d2b5f34443f234f62ee109cc509cdac0615b
Shyaboi <ian.shane.sears@gmail.com>
2021-01-21 07:43:35 -0700

    changed server

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 9001
+
 let root = path.join(__dirname, 'build/')
+app.use(express.static(__dirname + '/public'));
+
+
 app.use(express.static(root))
-app.use(function(req, res, next) {
-  if (req.method === 'GET' && req.accepts('html') && !req.is('json') && !req.path.includes('.')) {
-    res.sendFile('index.html', { root })
-  } else next()
-})
+
+
 
 
 app.get('/new/code-editor', function (req, res) {
