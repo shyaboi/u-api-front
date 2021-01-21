75c6da483db7028ba1e2ba8c556efc1bc2e0fc6e
Shyaboi <ian.shane.sears@gmail.com>
2021-01-21 07:38:25 -0700

    added port processenv to server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const PORT = process.env.PORT || 9001
 let root = path.join(__dirname, 'build/')
 app.use(express.static(root))
 app.use(function(req, res, next) {
@@ -15,4 +15,4 @@ app.get('/new/code-editor', function (req, res) {
    res.sendFile(path.join(__dirname, root, 'index.html'));
  });
 
-app.listen(9001);
+app.listen(PORT);
