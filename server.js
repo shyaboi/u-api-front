9cfbcbc5a1ace1a4856d154700de78c9e4328a78
Shyaboi <ian.shane.sears@gmail.com>
2021-01-21 07:47:39 -0700

    routing properly to route and not home

--- server.js
+++ server.js
@@ -3,17 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 9001
 
-let root = path.join(__dirname, 'build/')
 app.use(express.static(__dirname + '/public'));
 
-
-app.use(express.static(root))
-
-
-
-
-app.get('/new/code-editor', function (req, res) {
-   res.sendFile(path.join(__dirname, root, 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/new/code-editor*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
 
 app.listen(PORT);
