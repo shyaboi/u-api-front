ef6ea50bea8b8a5310928fdba6eaf53481310c9e
Shyaboi <ian.shane.sears@gmail.com>
2021-01-21 07:31:35 -0700

    adding more pathing to the build folder

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+let root = path.join(__dirname, 'build/')
+app.use(express.static(root))
+app.use(function(req, res, next) {
+  if (req.method === 'GET' && req.accepts('html') && !req.is('json') && !req.path.includes('.')) {
+    res.sendFile('index.html', { root })
+  } else next()
+})
 
-app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/new/code-editor', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
